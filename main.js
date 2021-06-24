910a0a15acea149609bdcbe93731b498a482edf7
RR-Coder-1210 <84138289+RR-Coder-1210@users.noreply.github.com>
2021-06-24 19:54:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+menu_list_array = ["Veg Margherita Pizza",//add more items
+"Chicken Tandoori Pizza",
+"Mushroom Gralic Pizza(Worst Pizza On Our List,Please Don't Eat It)", 
+"Mexican Chicken",
+"Kebab Pizza",
+"Indian Pizza(Best Pizza On Our List, Please Eat It)"];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
